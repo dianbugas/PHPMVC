080cc5802a99ae737b543da6a428dfc8536dbb8d
dianynf <dianbugas@gmail.com>
2018-11-19 11:24:25 +0700

    penambahan code

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -41,10 +41,21 @@
 			<label for="email">Email</label>
 			<input type="email" class="form-control" id="email" name="email">
 		</div>
+		<div class="form-group">
+			<label for="jurusan">Jurusan</label>
+			<select class="form-control" id="jurusan" name="jurusan">
+			<option value="Teknik Informatika">Teknik Informatika</option>
+			<option value="Teknik Mesin">Teknik Mesin</option>
+			<option value="Teknik Industri">Teknik Industri</option>
+			<option value="Teknik Pangan">Teknik Pangan</option>
+			<option value="Teknik Plonologi">Teknik Plonologi</option>
+			<option value="Teknik Lingkungan">Teknik Lingkungan</option>
+			</select>
+		</div>
       	</div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Tambah data mahasiswa</button>
+        <button type="submit" class="btn btn-primary">Tambah data mahasiswa</button>
 		</form>
 	  </div>
     </div>
